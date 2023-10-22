dba88da43d759e957c2c87bfb73b8e234990cb0d
skyrimYun <skyrim-yy@outlook.com>
2023-10-22 17:20:35 +0800

    update insertsort

--- sort.cpp
+++ sort.cpp
@@ -25,15 +25,14 @@ void bubblesort(T arr[], int len)
 template <typename T>
 void insertsort(T arr[], int len)
 {
-    for (int i = 0; i < len; i++)
-    {
-        int j = len - 1;
-        while (j != i)
-        {
-            if (arr[j] < arr[i])
-                swap(arr[j], arr[i]);
+    for(int i=1;i<len;i++) {
+        int key=arr[i];
+        int j=i-1;
+        while((j>=0) && (key<arr[j])) {
+            arr[j+1]=arr[j];
             j--;
         }
+        arr[j+1]=key;
     }
 }
 
