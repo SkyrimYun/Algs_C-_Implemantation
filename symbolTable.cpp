76ee383d538e0cc5745dd9091fe9d675e9252b42
skyrimYun <skyrim-yy@outlook.com>
2023-10-29 12:48:09 +0800

    improve BST

--- symbolTable.cpp
+++ symbolTable.cpp
@@ -1,32 +1,133 @@
 #include <iostream>
 #include <vector>
+#include <memory>
+
 using namespace std;
 
-template <class Key, class Value>
-class BST
-{
-private:
-    struct Node
-    {
+template<class Key, class Value>
+class BST {
+public:
+
+    struct Node {
         Key key;
         Value value;
         int count = 1;
-        Node *left;
-        Node *right;
-        Node(key k, value val)
-        {
-            this.key = k;
-            this.value = val;
+        shared_ptr<Node> left;
+        shared_ptr<Node> right;
+
+        Node(Key k, Value val) {
+            this->key = k;
+            this->value = val;
         }
-        Node(){};
+
+        Node() = default;
     };
+    using NodePtr = shared_ptr<Node>;
 
-    Node *root;
+    BST() = default;
 
-    Node *put(Node *x, Key key, Value val)
-    {
-        if (x == nullptr)
-            return new Node(key, val);
+    void put(Key key, Value val) {
+        root = put(root, key, val);
+    }
+
+    Value get(Key key) {
+        NodePtr x = root;
+        while (x != nullptr) {
+            if (key > x->key) {
+                x = x->right;
+            } else if (key < x->key) {
+                x = x->left;
+            } else {
+                return x->value;
+            }
+        }
+
+        return Value();
+    }
+
+    void deleteKey(Key key) {
+        root = deleteKey(root, key);
+    }
+
+    bool contains(Key key) {
+        NodePtr x = root;
+        while (x != nullptr) {
+            if (key > x->key) {
+                x = x->right;
+            } else if (key < x->key) {
+                x = x->left;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool isEmpty() {
+        return size(root) == 0;
+    }
+
+    int size() {
+        return size(root);
+    }
+
+    Key min() {
+        return min(root);
+    }
+
+    Key max() {
+        return max(root);
+    }
+
+    // largest key ≤ a given key.
+    Key floor(Key key) {
+        NodePtr x = floor(root, key);
+        if (x == nullptr) {
+            return Key();
+        }
+        return x->key;
+    }
+
+    // Smallest key ≥ a given key.
+    Key ceiling(Key key) {
+        NodePtr x = ceiling(root, key);
+        if (x == nullptr) {
+            return Key();
+        }
+        return x->key;
+    }
+
+    //How many keys < key
+    int rank(Key key) {
+        return rank(root, key);
+    }
+
+    // the key that ranks k th
+    Key select(int k) {
+        return select(root, k);
+    }
+
+    void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    void deleteMax() {
+        root = deleteMax(root);
+    }
+
+    vector<Key> keys() {
+        vector<Key> k;
+        inorder(k, root);
+        return k;
+    }
+
+
+private:
+
+    NodePtr put(NodePtr x, Key key, Value val) {
+        if (x == nullptr) {
+            return make_shared<Node>(key, val);
+        }
 
         if (key > x->key)
             x->right = put(x->right, key, val);
@@ -40,158 +141,171 @@ private:
         return x;
     }
 
-    Node *floor(Node *x, Key key)
-    {
-        if (x == nullptr)
+    NodePtr floor(NodePtr x, Key key) {
+        if (x == nullptr) {
             return nullptr;
+        }
 
-        //Case 1. [k is less than the key at root]
-        //The floor of k is in the left subtree.
-        if (key < x->key)
-            x = floor(x->left, key);
+        //Case 1. [key is less than the value at root]
+        //The floor of key is in the left subtree.
+        if (key < x->key) {
+            return floor(x->left, key);
+        }
 
-        //Case 2. [k equals the key at root]
-        //The floor of k is k.
-        if (key == x->key)
+        //Case 2. [key equals the root]
+        //The floor of key is root.
+        if (key == x->key) {
             return x;
+        }
 
-        //Case 3. [k is greater than the key at root]
-        //The floor of k is in the right subtree(if there is any key ≤ k in right subtree);
-        //otherwise it is the key in the root.
-        Node *t = x->right;
+        //Case 3. [key is greater than the value at root]
+        //The floor of key is in the right subtree(if there is any value ≤ key in right subtree);
+        //otherwise it is the value in the root.
+        NodePtr t = x->right;
         t = floor(t, key);
-        if (t != nullptr)
+        if (t != nullptr) {
             return t;
-        else
+        } else {
             return x;
+        }
     }
 
-    Node *ceiling(Node *x, Key key)
-    {
-        if (x == nullptr)
+    NodePtr ceiling(NodePtr x, Key key) {
+        if (x == nullptr) {
             return nullptr;
+        }
 
-        if (key > x->key)
-            x = ceiling(x->ritgt, key);
+        if (key > x->key) {
+            return ceiling(x->ritgt, key);
+        }
 
-        if (key == x->key)
+        if (key == x->key) {
             return x;
+        }
 
-        Node *t = x->left;
+        NodePtr t = x->left;
         t = ceiling(t, key);
-        if (t != nullptr)
+        if (t != nullptr) {
             return t;
-        else
+        } else {
             return x;
+        }
     }
 
-    int size(Node *x)
-    {
-        if (x == nullptr)
+    int size(Node* x) {
+        if (x == nullptr) {
             return 0;
+        }
+
         return x->count;
     }
 
-    int rank(Node *x, Key key)
-    {
-        if (x == nullptr)
+    int rank(NodePtr x, Key key) {
+        if (x == nullptr) {
             return 0;
+        }
 
-        if (key > x->key)
+        if (key > x->key) {
             return 1 + size(x->left) + rank(x->right, key);
-        else if (key < x->key)
+        } else if (key < x->key) {
             return rank(x->left, key);
-        else
+        } else {
             return size(x->left);
+        }
     }
 
-    Key select(Node *x, int k)
-    {
-        if (x == nullptr)
+    Key select(NodePtr x, int k) {
+        if (x == nullptr) {
             return Key();
+        }
 
-        if (k > rank(x->key))
+        if (k > rank(x->key)) {
             return select(x->right, k);
-        else if (k < rank(x->key))
+        } else if (k < rank(x->key)) {
             return select(x->left, k);
-        else
+        } else {
             return x->key;
+        }
     }
 
-    void inorder(vector<Key> &keys, Node *x)
-    {
-        if (x == nullptr)
+    void inorder(vector<Key> &keys, NodePtr x) {
+        if (x == nullptr) {
             return;
+        }
         inorder(keys, x->left);
         keys.push_back(x->key);
         inorder(keys, x->right);
     }
 
-    Node *deleteMin(Node *x)
-    {
-        if (x->left == nullptr)
+    NodePtr deleteMin(NodePtr x) {
+        if (x->left == nullptr) {
             return x->right;
+        }
 
         x->left = deleteMin(x->left);
-        x.count = 1 + size(x->left) + size(x->right);
+
+        x->count = 1 + size(x->left) + size(x->right);
         return x;
     }
 
-    Node *deleteMax(Node *x)
-    {
+    Node* deleteMax(Node* x) {
         if (x->right == nullptr)
             return x->left;
 
         x->right = deleteMax(x->right);
-        x.count = 1 + size(x->left) + size(x->right);
+        x->count = 1 + size(x->left) + size(x->right);
         return x;
     }
 
-    Key min(Node *x)
-    {
-        if (x == nullptr)
+    Key min(NodePtr x) {
+        if (x == nullptr) {
             return nullptr;
+        }
 
-        while (x->left != nullptr)
+        while (x->left != nullptr) {
             x = x->left;
+        }
         return x->key;
     }
 
-    Key max(Node *x)
-    {
-        if (x == nullptr)
+    Key max(Node* x) {
+        if (x == nullptr) {
             return nullptr;
+        }
 
-        while (x->right != nullptr)
-            x = x - right;
+        while (x->right != nullptr) {
+            x = x->right;
+        }
+
         return x->key;
     }
 
-    Node *deleteKey(Node *x, Key key)
-    {
-        if (x == nullptr)
+    NodePtr deleteKey(NodePtr x, Key key) {
+        if (x == nullptr) {
             return nullptr;
+        }
 
         // Hibbard deletion
         // search for key
-        if (key > x->key)
+        if (key > x->key) {
             x->right = deleteKey(x->right, key);
-        else if (key < x->key)
+        } else if (key < x->key) {
             x->left = deleteKey(x->left, key);
-        else
-        {
+        } else {
             //no right child
-            if (x->left == null)
+            if (x->left == nullptr) {
                 return x->right;
+            }
             // no left child
-            if (x->right == null)
+            if (x->right == nullptr) {
                 return x->left;
+            }
 
             //have two children
             //use the min on the right subtree to replace
-            Node *t = x;
+            NodePtr t = x;
             x = min(t->right);
-            x->right = deleteMin(t->right, key);
+            x->right = deleteMin(t->right);
             x->left = t->left;
         }
 
@@ -200,121 +314,9 @@ private:
         return x;
     }
 
-public:
-    ST(){};
+private:
 
-    void put(Key key, Value val)
-    {
-        root = put(root, key, val);
-    }
-
-    Value get(Key key)
-    {
-        Node *x = root;
-        while (x != nullptr)
-        {
-            if (key > x->key)
-                x = x->right;
-            else if (key < x->key)
-                x = x->left;
-            else
-                return x->value;
-        }
-        return Value();
-    }
-
-    void deleteKey(Key key)
-    {
-        root = deleteKey(root, key);
-    }
-
-    bool contains(Key key)
-    {
-        Node *x = root;
-        while (x != nullptr)
-        {
-            if (key > x->key)
-                x = x->right;
-            else if (key < x->key)
-                x = x->left;
-            else
-                return true;
-        }
-        return false;
-    }
-
-    bool isEmpty()
-    {
-        return size(root) == 0;
-    }
-
-    int size()
-    {
-        return size(root);
-    }
-
-    Key min()
-    {
-        return min(root);
-    }
-
-    Key max(){
-        return max(root)}
-
-    // largest key ≤ a given key.
-    Key floor(Key key)
-    {
-        Node *x = floor(root, key);
-        if (x == nullptr)
-            return Key();
-        return x->key;
-    }
-
-    // Smallest key ≥ a given key.
-    Key ceiling(Key key)
-    {
-        Node *x = ceiling(root, key);
-        if (x == nullptr)
-            return Key();
-        return x->key;
-    }
-
-    //How many keys < key
-    int rank(Key key)
-    {
-        return rank(root, key);
-    }
-
-    // the key that ranks k th
-    Key select(int k)
-    {
-        return select(root, k);
-    }
-
-    void deleteMin()
-    {
-        root = deleteMin(root);
-    }
-
-    void deleteMax()
-    {
-        root = deleteMax(root);
-    }
-
-    vector<Key> keys()
-    {
-        vector<Key> k;
-        inorder(k, root);
-        return k;
-    }
+    NodePtr root = nullptr;
 };
 
-int main()
-try
-{
-    /* code */
-}
-catch (const std::exception &e)
-{
-    std::cerr << e.what() << '\n';
-}
+
